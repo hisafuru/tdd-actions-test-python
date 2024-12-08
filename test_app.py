16f778514c5e33613757ff1607b46c0ba020e35d
Kota Hisafuru <kyuko.hisa@gmail.com>
2024-12-08 16:19:25 +0900

    テストケースを追加

--- test_app.py
+++ test_app.py
@@ -6,3 +6,4 @@ def test_double_number():
     assert double_number(-3) == -6  # 負の値
     assert double_number(0) == 0  # ゼロ
     assert double_number(100) == 200  # 大きな数
+    assert double_number(-100) == -200  # 負の大きな数
